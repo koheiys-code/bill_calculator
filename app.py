0b2c185e7e04fb5ad7e8659ce9ad5c4921372360
koheiys-code <kohei7.22wimper@gmail.com>
2022-09-05 20:53:35 +0900

    2022/9/5

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ st.title("払う金額を計算します")
 
 lines = st.text_area("'名前、目的、金額'を縦に並べて入力してください")
 if lines:
-    lines = lines.split(' ')
-    bill_dict = defaultdict(list)
-    for line in lines:
-        st.write('#####')
-        st.write(line)
-        st.write('#####')
-        name, purpose, bill = line.split('、')
-        bill_dict[name].append([purpose, bill])
+    st.write(lines)
+    # bill_dict = defaultdict(list)
+    # for line in lines:
+    #     st.write('#####')
+    #     st.write(line)
+    #     st.write('#####')
+    #     name, purpose, bill = line.split('、')
+    #     bill_dict[name].append([purpose, bill])
 
-    for name, contents in bill_dict.items():
-        st.write(name)
-        for content in contents:
-            content = ' '.join(content)
-            st.write('\t'+content)
+    # for name, contents in bill_dict.items():
+    #     st.write(name)
+    #     for content in contents:
+    #         content = ' '.join(content)
+    #         st.write('\t'+content)
